0d485c97093028c12bfb45d8f0fde5ca875860cc
dee homak <dhomak@users.noreply.github.com>
2026-07-09 07:09:06 +0300

    Initial Commit

---
+++ MAMEFrontend/MAMEFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAMEFrontendApp.swift
+//  MAMEFrontend
+//
+//  Created by dee homak on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MAMEFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
